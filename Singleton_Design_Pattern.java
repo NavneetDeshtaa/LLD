eea5dea2c0adeca2f4dd95dc2676cafbc1776f53
Navneet Deshta <navneetdeshtaa@gmail.com>
2026-08-28 17:10:57 +0530

    Refactor Example class to implement double-checked locking for singleton instance creation

--- Singleton_Design_Pattern.java
+++ Singleton_Design_Pattern.java
@@ -1,14 +1,23 @@
-class Example{
-    private static Example instance = null;
-    
-    private Example(){
+class Example {
+
+    private static volatile Example instance = null;
+
+    private Example() {
         System.out.println("Constructor Called");
     }
-    
-    public static Example getInstance(){
-        if(instance == null){
-            instance = new Example();
+
+    public static Example getInstance() {
+
+        if (instance == null) {
+
+            synchronized (Example.class) {
+
+                if (instance == null) {
+                    instance = new Example();
+                }
+            }
         }
+
         return instance;
     }
 }
